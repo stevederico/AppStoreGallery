adddd7c45b84c3572a11c72426c9e02a2cf1eaeb
Steve Derico <Steve@SteveDerico.com>
2011-12-14 11:54:34 -0800

    Initial Commit

---
+++ AppStoreGallery/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AppStoreGallery
+//
+//  Created by Stephen Derico on 12/14/11.
+//  Copyright (c) 2011 Bixby Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AppStoreGallery/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AppStoreGallery
+//
+//  Created by Stephen Derico on 12/14/11.
+//  Copyright (c) 2011 Bixby Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
